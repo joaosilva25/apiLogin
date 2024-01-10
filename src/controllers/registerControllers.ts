e41bc25f66648dcc4afedecd7ea9f40dae20b1ac
JoaoSilva25 <joaosilva2505@outlook.com>
2024-01-10 15:42:19 -0300

    add validator

--- src/controllers/registerControllers.ts
+++ src/controllers/registerControllers.ts
@@ -1,6 +1,7 @@
 import {Request,Response} from 'express';
 import bcrypt from 'bcryptjs';
 import Users from '../models/Users';
+import validator from "validator";
 
 export const registerUser = async(req: Request, res: Response) => {
     const {userName,email,password}=req.body;
@@ -8,23 +9,29 @@ export const registerUser = async(req: Request, res: Response) => {
 
     try {   
         if(userName && email && password) {
+
             const searchUser=await Users.findOne({email,userName});
             const hashPassword=bcrypt.hashSync(password,10);
 
             if(!searchUser) {
-                const newUser=await Users.create({
-                    userName,
-                    email,
-                    password:hashPassword
-                })
-                res.status(201).json({user:newUser});
+                if(validator.isEmail(email)) {
+                    const newUser=await Users.create({
+                        userName,
+                        email,
+                        password:hashPassword
+                    })
+                    res.status(201).json({user:newUser});
+                }
+                else {
+                    res.status(422).json({mensage:"Endereço de Email Inválido"})
+                }
             }
             else {
                 res.status(409).json({mensage:"Usuario já registrado"});
             }
         }
         else {
-            res.json(400).json({mensage:"Preencha os campos solicitados"})
+            res.status(400).json({mensage:"Preencha os campos solicitados"})
         }
 
     }
--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,16 +1,18 @@
-import {connect} from "mongoose";
+import {ConnectOptions, connect} from "mongoose";
 import dotenv from "dotenv";
 
 
 dotenv.config();
 
 export const mongoConnect=async()=> {
+    console.log('String de Conexão:', process.env.MONGO_DB);
+
     try {
-        await connect(process.env.MONGO_DB as string);
+        await connect(process.env.MONGO_DB as string)
         console.log("MONGODB Conectado");
     }
     catch (err) {
-        console.log("Não conectado ao MONGODB");
+        console.log(err);
     }
 }
 
