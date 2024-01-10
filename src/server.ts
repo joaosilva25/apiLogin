1ca299cbbdb3735ec77467a3d785d0528ac01931
JoaoSilva25 <joaosilva2505@outlook.com>
2024-01-09 21:40:36 -0300

    development 1.0.1

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 import path from 'path';
 import ApiRoutes from './routes/apiRoutes';
 import { mongoConnect } from './database/mongo';
+import cors from 'cors'
 
 
 dotenv.config();
 const app = express();
+
 mongoConnect();
 
+app.use(cors())
 app.use(express.static(path.join(__dirname,"./public")));
 app.use(express.urlencoded({extended:true}));
 
